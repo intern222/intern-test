76383a5ae9f24e7a2f65eacfd470478a635e607e
intern222 <internpda@gmail.com>
2021-03-12 16:46:27 +0000

    footer fix

--- UI/src/components/Footer.js
+++ UI/src/components/Footer.js
@@ -7,9 +7,11 @@ function Footer() {
   return (
     <div className='footer-container'>
       <div className='footer-links'>
-        <div className='footer-link-items'>
-          <h2>About Us</h2>
-          <Link to='/contacts'>Contactos</Link>
+        <div className="footer-link-wrapper">
+          <div className='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/contacts'>Contactos</Link>
+          </div>
         </div>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
